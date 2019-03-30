8200ff10f1404acfe6f42b883eadd30dac99fb47
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-03-30 13:27:40 +0100

    add and get challenges

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import json
 from tinydb import TinyDB, Query
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
-db = TinyDB('challenges.json')
+db: TinyDB = TinyDB('challenges.json')
+q: Query = Query()
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
 @app.route("/challenges")
 def get_all_challenges():
     return json.dumps(db.all())
+
+
+@app.route("/challenge", methods=['POST'])
+def add_challenge():
+    new_challenge = request.json
+    db.upsert(new_challenge, q.challenge_id == new_challenge['challenge_id'])
+    return "ok"
