f9808829e858b2b0866d2bbe50ed17d646052f29
Jonas Scholz <jonas.scholz123@gmail.com>
2019-03-30 12:13:12 +0000

    create tinydb

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import json
+from tinydb import TinyDB, Query
+
 from flask import Flask
 app = Flask(__name__)
-
+db = TinyDB('challenges.json')
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/challenges")
+def get_all_challenges():
+    return json.dumps(db.all())
